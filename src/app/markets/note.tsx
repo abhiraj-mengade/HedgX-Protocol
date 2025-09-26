1a4dbd9cd135478eee23507e759360880c2bc500
pranavcpawar <pranavcpawar@outlook.com>
2025-09-27 04:55:50 +0530

    add footer component

---
+++ src/app/markets/note.tsx
@@ -0,0 +1,7 @@
+export default function Note() {
+  return (
+    <footer className="my-2 text-center text-xs text-[#6b6b6b] font-outfit md:fixed  bottom-0 w-full bg-background sticky">
+      © 2025 HedgX Protocol. All rights reserved.
+    </footer>
+  );
+}
